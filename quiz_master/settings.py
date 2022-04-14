a220d034e44a8a898fc4bb09cd81d710138ef5b6
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-14 11:53:38 +0300

    Updated static path

--- quiz_master/settings.py
+++ quiz_master/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
