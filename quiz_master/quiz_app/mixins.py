a3f150abaf1cc1217c08d5f8a7240b66b0e04f1b
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-16 09:40:38 +0300

    Update edit view security

--- quiz_master/quiz_app/mixins.py
+++ quiz_master/quiz_app/mixins.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views import generic as views
 from quiz_master.quiz_app.forms import QuestionForm, AnswerForm, QuizForm, StatsForm
 from quiz_master.quiz_app.models import Question, Answer
@@ -5,6 +6,10 @@ from quiz_master.quiz_app.models import Question, Answer
 
 class GetQuizWithDataMixin(views.UpdateView):
     def get(self, *args, **kwargs):
+        if 1 != len(self.request.user.groups.filter(name='moderator')):
+            if self.get_object().author.id != self.request.user.id:
+                return redirect('quizzes')
+
         questions_formset = []
         answers_formset = []
 
