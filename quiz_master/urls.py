f649efaaa962de7420e6cc12e77c7e5b00292508
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2022-04-19 21:00:38 +0300

    Added sitemap

--- quiz_master/urls.py
+++ quiz_master/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
+from django.contrib import admin, sitemaps
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz_master.quiz_app.urls')),
     path('accounts/', include('quiz_master.accounts.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
